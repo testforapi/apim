7eec08d51ca3fe9e605d2c074044221f4fc6548d
chao.chang[張昭容] <chao.chang@104.com.tw>
2016-10-24 16:55:53 +0800

    script modified

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ function btn_getFile(){
 	var div_container = document.getElementById("container")
 	var div_content = document.createElement("div");
 	div_content.textContent = "Hello world";
+	while(div_container.firstChild){
+		div_container.removeChild(div_container.firstChild);
+	}
 	div_container.appendChild(div_content);
 }
