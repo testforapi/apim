b86fda8eff7703d6c4796971c45ce35ffd21bb8f
chao.chang[張昭容] <chao.chang@104.com.tw>
2016-10-24 16:47:08 +0800

    index and js modified

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function btn_getFile(){
-	var div = document.createElement("div");
-	div.textContent = "Hello world";
+	var div_container = document.getElementById("container")
+	var div_content = document.createElement("div");
+	div_content.textContent = "Hello world";
+	div_container.appendChild(div_content);
 }
